eb63c7787cb2d288e6d284cc6427999e481bdc33
realtroffy <artur.asiptsov@yandex.ru>
2025-11-20 13:02:30 +0300

    add main functionality

---
+++ src/test/kotlin/com/innowise/activityservice/ActivityServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.innowise.activityservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
